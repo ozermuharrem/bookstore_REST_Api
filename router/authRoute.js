079a82e2367a159507e913fe3d79fba7ab211db4
ozermuharrem <muharremozer505@gmail.com>
2023-03-13 10:21:52 +0300

    readme

--- router/authRoute.js
+++ router/authRoute.js
@@ -4,7 +4,7 @@ const checkJwt = require('../middlewares/authMiddlewares');
 
 const router = express.Router();
 
-router.route('/signup').post(authController.createUser);
+router.route('/register').post(authController.createUser);
 router.route('/login').post(authController.login);
 router.route('/logout').get(authController.logoutUser);
 router.route('/:_id').delete(authController.deleteUser);
