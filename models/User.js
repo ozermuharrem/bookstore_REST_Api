369374b6f105fb3534a3182812a00d40015242eb
Muharrem ÖZER <86782430+ozermuharrem@users.noreply.github.com>
2023-03-17 23:06:55 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
-
 const Schema = mongoose.Schema;
 
+// user Schema
 const userSchema = new Schema ({
     email : {
         type : String,
