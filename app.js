581b3ae76879ca6c95fbbbe3f5641e1e8a6cddd9
Muharrem ÖZER <86782430+ozermuharrem@users.noreply.github.com>
2023-03-17 23:04:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 
 // Mongodb connect 
-mongoose.connect('mongodb+srv://muharremozer505:GMTKPdfpa5B9GnZZ@cluster0.ulqw79t.mongodb.net/bookstore?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://muharremozer505:********@cluster0.ulqw79t.mongodb.net/bookstore?retryWrites=true&w=majority')
 .then(()=>{
     console.log('DB Connected Successfully')
 })
@@ -23,10 +23,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 
 //Router
-/*
-app.use('/', (req,res)=>{
-	res.send('Welcome To Api Server');
-}); */
 app.use('/user', authRoute);
 app.use('/books',bookRoute);
 
